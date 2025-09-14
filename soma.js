b50b082a629285a74a2bc315c4f339840713b6dd
Ademir <eucrioomeufuturo@gmail.com>
2025-09-14 05:04:33 -0300

    código para operações matemáticas.

---
+++ soma.js
@@ -0,0 +1,35 @@
+* soma.js
+* subtracao.js
+* multiplicacao.js
+* divisao.js
+* radiciacao.js
+
+// Soma em Javascript 
+var x = 5; 
+var y = 5; 
+var g = 5 + 5 
+console.log("g = " + g); 
+// Subtração em Javascript 
+var c = 45; 
+var b = 5; 
+var y = 45 - 5 
+console.log("y = " + y); 
+// Multiplicação em Javascript 
+var x = 7; 
+var y = 7; 
+var l = 7 * 7 
+console.log("l = " + l); 
+// Divisão em Javascript 
+var x = 16; 
+var y = 2; 
+var p = 16 / 2 
+console.log("p = " + p); 
+// Potencia em Javascript 
+var f = 10; 
+var t = 2; 
+var y = 10 ** 2 
+console.log("y = " + y); 
+// Raiz quadrada em Javascript 
+var f = 56; 
+var g = Math.sqrt(f) 
+console.log("g = " + g)
